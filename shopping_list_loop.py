a549cbd760a0e62b608a42c8ebeecc5ac36f17dc
Katie Freeman <katiefreeman@carltons-mbp.lan>
2021-12-14 20:52:48 -0500

    add exceptions

--- shopping_list_loop.py
+++ shopping_list_loop.py
@@ -1,21 +1,7 @@
+from shoping_list import ShoppingList
+from shoping_list import GroceryItem
 
-shopping_lists = [] 
-
-class GroceryItem: 
-  def __init__(self, name): 
-    self.name = name 
-
-
-class ShoppingList: 
-  def __init__(self, name, address): 
-    self.name = name 
-    self.address = address 
-    self.grocery_items = []
-  def __str__(self):
-        return ""
-  
-  def add_grocery_item(self, item):
-    self.grocery_items.append(item)
+shopping_lists = []
     
 
 def display_all_shopping_lists():
@@ -28,42 +14,60 @@ def display_all_shopping_lists():
             print(f"{index+1}.{shopping_list.grocery_items[index].name}")
     
 
-def print_menu_error():
-    print("That was not a valid choice. Try again. \n\n\n")
-
 def main():
     
     while True:
-
-        print("1. Add a shopping list")
-        print("2. Add item to an existing shopping list")
-        print("3. Display shopping lists")
-        print("q to quit")
-
-        choice = input("Enter choice: ")
-
-        if choice == "1": 
-            display_all_shopping_lists() 
-            name = input("Enter name of shopping list: ")
-            address = input("Enter shopping list address: ")
+        try:
+            print("1. Add a shopping list")
+            print("2. Add item to an existing shopping list")
+            print("3. Display shopping lists")
+            print("4. Delete an item from shopping list")
+            print("q to quit")
             
-            shopping_list = ShoppingList(name, address)
-            shopping_lists.append(shopping_list)
-        
-        elif choice == "2":
-            print("Which list would you like to add to?")
-            display_all_shopping_lists()
-            list_choice = int(input ("Enter choice: "))
-            shopping_list = shopping_lists[list_choice -1]
+            choice = input("Enter choice: ")
+            if choice == "1": 
+                display_all_shopping_lists() 
+                name = input("Enter name of shopping list: ")
+                address = input("Enter shopping list address: ")
+                    
+                shopping_list = ShoppingList(name, address)
+                shopping_lists.append(shopping_list)
+            
+            elif choice == "2":
+                try:
+                    print("Which list would you like to add to?")
+                    display_all_shopping_lists()
+                    list_choice = int(input ("Enter Choice: "))
+                    shopping_list = shopping_lists[list_choice -1]
 
-            name = input("Enter grocery item: ")
-            grocery_item = GroceryItem(name)
+                    name = input("Enter grocery item: ")
+                    grocery_item = GroceryItem(name)
 
-            shopping_list.add_grocery_item(grocery_item)
+                    shopping_list.add_grocery_item(grocery_item)
+                except ValueError:
+                    print("Please select a number")
 
-        elif choice == "3":
-            display_all_shopping_lists()
-           
-        elif choice == "q":
-            break
+            elif choice == "3":
+                display_all_shopping_lists()
+
+            elif choice == "4":
+                try:
+                    display_all_shopping_lists()
+                    list_choice = int(input ("Enter choice: "))
+                    shopping_list = shopping_lists[list_choice -1]
+                    item_choice = int(input ("Enter item choice: "))
+                    shopping_list.delete_grocery_item(item_choice -1)
+                    display_all_shopping_lists()
+                except ValueError:
+                    print("Please select a number")
+
+
+            elif choice == "q":
+                break
+
+        except IndexError:
+            print("This is not a valid selection")
+    
+
+
 main()
