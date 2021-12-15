c05e9e63bcbda28b852cff174dd1dd6151fa30e9
Katie Freeman <katiefreeman@carltons-mbp.lan>
2021-12-14 22:12:50 -0500

    add test

--- shoping_list.py
+++ shoping_list.py
@@ -1,36 +1,27 @@
+class GroceryItem: 
+  def __init__(self, name): 
+    self.name = name 
 
-shopping_list = []
 
-class GroceryItem:
-    def __init__(self, name):
-        self.name = name
+class ShoppingList: 
+  def __init__(self, name, address): 
+    self.name = name 
+    self.address = address 
+    self.grocery_items = []
+  def __str__(self):
+        return ""
 
-class ShoppingList:
-    def __init__(self, name, address):
-        self.name = name
-        self.address = address
-        self.grocery_items = []
-    
-    def add_grocery_item(self, item):
-        self.grovery_items.append(item)
+  def add_grocery_item(self, item):
+    self.grocery_items.append(item)
 
-def display_all_shopping_lists(): 
+  def delete_grocery_item(self, index):
+    del self.grocery_items[int(index)]
+
+  def display_all_shopping_lists():
     for index in range(0, len(shopping_lists)):
-       shopping_list = shopping_lists[index]
-       print(f"{index}. {shopping_list.name}")
-  
-print("1. Add a shopping list")
-print("2. add item to an existing shopping list")
-print("3. display shopping lists")
-print("q to quit")
-
-choice = input("Enter choice: ")
-
-if choice == "1":
-    display_all_shopping_lists()
-    name = input("Enter name of shopping list: ")
-    address = input("Enter shopping list address: ")
-    shopping_list = ShoppingList(name, address)
-    shopping_lists.append(shopping_list)
-elif choice == "3":
-    display_all_shopping_lists()
+        shopping_list = shopping_lists[index]
+        print("===============")
+        print(f"{shopping_list.name}")
+        print("===============")
+        for index in range(0, len(shopping_list.grocery_items)):
+            print(f"{index+1}.{shopping_list.grocery_items[index].name}")
---
+++ test_shopping_list.py
@@ -0,0 +1,28 @@
+import unittest
+from shoping_list import ShoppingList
+from shoping_list import GroceryItem
+
+class ShoppingListTest(unittest.TestCase): 
+    
+    def setUp(self):
+        print("SETUP")
+        self.shopping_list = ShoppingList("Kroger", "234 Road")
+        self.shopping_list
+
+    def test_add_grocery_item(self):
+        self.shopping_list.add_grocery_item("banana")
+        self.shopping_list.add_grocery_item("milk")
+        self.assertEqual(2, len(self.shopping_list.grocery_items))
+
+    def test_delete_grocery_item(self):
+        self.shopping_list.add_grocery_item("banana")
+        self.shopping_list.add_grocery_item("milk")
+        self.shopping_list.delete_grocery_item(1)
+        self.assertEqual(5, len(self.shopping_list.grocery_items))
+
+
+    def tearDown(self):
+        print("TEARDOWN")
+
+if __name__ == '__main__':
+    unittest.main()
