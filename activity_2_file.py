c50d1801cf3ae605aeb2bf3150a764da40fd02da
katiefreeman0611 <50997643+katiefreeman0611@users.noreply.github.com>
2021-12-17 15:06:25 -0500

    Delete activity_2_file.py

--- activity_2_file.py
+++
@@ -1,7 +0,0 @@
-with open("programing.txt", "a") as file:
-    while True:
-        answer = input("What do you like about programing? press q to quit: ")
-        if answer == "q":
-            break
-        file.write(answer)
-        file.write("\n")
