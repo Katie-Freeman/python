c8cebb7ba3ebd3cd016da11dc0a4f08ed7cb54ad
katiefreeman0611 <50997643+katiefreeman0611@users.noreply.github.com>
2021-12-17 15:10:04 -0500

    Delete employee_raise.py

--- employee_raise.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    def __init__(self, employee_name, annual_salary):
-        self.employee = employee_name
-        self.salary = annual_salary 
-    
-    def give_raise(self, amount=5000):
-        self.salary += amount
-
